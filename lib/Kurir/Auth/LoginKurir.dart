968618d06dc271a96d8d9b8d594ddc5fdfee68a8
NIZAR RASYIID <nizarrasyiid1221@gmail.com>
2024-10-30 09:14:54 +0700

    Update Navigation

--- lib/Kurir/Auth/LoginKurir.dart
+++ lib/Kurir/Auth/LoginKurir.dart
@@ -107,11 +107,13 @@ class _LoginKurirState extends State<LoginKurir> {
                   onPressed: () {
                     if (_formKey.currentState!.validate()) {
                       // Process login
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  const MainKurirNavigation()));
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const MainKurirNavigation(),
+                        ),
+                        (Route<dynamic> route) => false,
+                      );
                     }
                   },
                   style: ElevatedButton.styleFrom(
